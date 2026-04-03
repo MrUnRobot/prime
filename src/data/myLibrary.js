ce8c6dd198ea7cc199397341c396ae1b4e69b648
MrUnRobot <mrunrobot@gmail.com>
2026-04-02 21:45:02 -0600

    Interfaz pulida: Nombres de capítulos automáticos y diseño Prime Pro

--- src/data/myLibrary.js
+++ src/data/myLibrary.js
@@ -2,20 +2,12 @@ export const mySeries = [
   {
     id: 1,
     tmdbId: "95557", // Invincible
-    temporada: 4,
+    temporada: 1, // Cambiado a 1 para que coincida con los datos reales de TMDB
     capitulos: [
-      { titulo: "Episodio 1", videoUrl: "https://photos.google.com/share/AF1QipO-A5GHAwl2YJNZuEpy5cudoDVUrPk62boNeUKouyYw-GxjwopVz0CGI5n1owHCUQ/photo/AF1QipPe2QfkmNBfsfkjlAPDwY9DH86w07mq8OiWP8aj?key=cDFGY3NoWEhyd0xxeFlSem1NUEJnVjhzaUhPN1B3" },
-      { titulo: "Episodio 2", videoUrl: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM216cR8RmNY9MDFcRfCaIUNWxnrPk13btIjSa7?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
-      { titulo: "Episodio 3", videoUrl: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM64LegJzut_wX6sIkOm07VRK7eqSaNR1vkAPym?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
-      { titulo: "Episodio 4", videoUrl: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipOugbRl2plTJscOyj_2gbq1AP3Isn3QCXSVgLIi?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" }
-    ]
-  },
-  {
-    id: 2,
-    tmdbId: "460668", // La casa de Jack
-    esPelicula: true,
-    capitulos: [
-      { titulo: "Ver Película", videoUrl: "" }
+      { videoUrl: "https://photos.google.com/share/AF1QipO-A5GHAwl2YJNZuEpy5cudoDVUrPk62boNeUKouyYw-GxjwopVz0CGI5n1owHCUQ/photo/AF1QipPe2QfkmNBfsfkjlAPDwY9DH86w07mq8OiWP8aj?key=cDFGY3NoWEhyd0xxeFlSem1NUEJnVjhzaUhPN1B3" },
+      { videoUrl: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM216cR8RmNY9MDFcRfCaIUNWxnrPk13btIjSa7?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+      { videoUrl: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM64LegJzut_wX6sIkOm07VRK7eqSaNR1vkAPym?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+      { videoUrl: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipOugbRl2plTJscOyj_2gbq1AP3Isn3QCXSVgLIi?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" }
     ]
   }
 ];
