98e44fa35a98a8f6b3a18bc8841276d3d4eea2e5
MrUnRobot <mrunrobot@gmail.com>
2026-04-04 18:08:15 -0600

    Data: Añadido Ash con link de Google Photos 6

--- src/data/myLibrary.js
+++ src/data/myLibrary.js
@@ -2,13 +2,26 @@ export const mySeries = [
   {
     id: "95557",
     tmdbId: "95557",
+    temporadaDefault: "4",
     temporadasDisponibles: [1, 2, 3, 4],
     capitulos: {
       "4": [
-        { id: 1, titulo: "El fin de todas las cosas: Parte 1", sinopsis: "La guerra contra los Viltrumitas alcanza su punto crítico mientras Mark toma una decisión imposible.", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipOjG1aeozRH08w9fpsc-_VdSLRRXrO9EgSZXky3?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
-        { id: 2, titulo: "Un nuevo orden mundial", sinopsis: "Las alianzas se rompen y la Tierra queda vulnerable ante una traición desde adentro.", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM216cR8RmNY9MDFcRfCaIUNWxnrPk13btIjSa7?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
-        { id: 3, titulo: "Legado de sangre", sinopsis: "Mark descubre la verdad sobre el linaje de su padre en un viaje al corazón del imperio.", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM64LegJzut_wX6sIkOm07VRK7eqSaNR1vkAPym?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
-        { id: 4, titulo: "Invencible: El capítulo final", sinopsis: "El enfrentamiento definitivo. El destino del universo se decide en un último aliento.", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipNX0Bz5F0Y-_rtAQohgYUq6GoujfJjEWBAuTUxF?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" }
+        { id: 1, titulo: "El fin de todas las cosas: Parte 1", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipOjG1aeozRH08w9fpsc-_VdSLRRXrO9EgSZXky3?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+        { id: 2, titulo: "Un nuevo orden mundial", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM216cR8RmNY9MDFcRfCaIUNWxnrPk13btIjSa7?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+        { id: 3, titulo: "Legado de sangre", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM64LegJzut_wX6sIkOm07VRK7eqSaNR1vkAPym?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+        { id: 4, titulo: "Invencible: El capítulo final", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipNX0Bz5F0Y-_rtAQohgYUq6GoujfJjEWBAuTUxF?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" }
+      ]
+    }
+  },
+  {
+    id: "ar-ash",
+    tmdbId: "64230", 
+    temporadaDefault: "1",
+    temporadasDisponibles: [1, 2, 3],
+    capitulos: {
+      "1": [
+        { id: 1, titulo: "El regreso de Ash", url: "https://photos.google.com/search/ChdBZ3JlZ2Fkb3MgcmVjaWVudGVtZW50ZSIIEgYKBHICCgAo3pTP19Uz/photo/AF1QipOz_RHrWy_TmsHwQfqFgTA7__1GlTmpJWRYHhFL" },
+        { id: 2, titulo: "Cebo", url: "https://photos.google.com/search/ChdBZ3JlZ2Fkb3MgcmVjaWVudGVtZW50ZSIIEgYKBHICCgAo3pTP19Uz/photo/AF1QipOz_RHrWy_TmsHwQfqFgTA7__1GlTmpJWRYHhFL" }
       ]
     }
   }
