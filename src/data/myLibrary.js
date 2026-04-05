6aee03e9ae7a5588df6a1323c13b620d4bcc2d40
MrUnRobot <mrunrobot@gmail.com>
2026-04-04 18:59:22 -0600

    Fix: Restaurados enlaces específicos para Invincible E03 y E04

--- src/data/myLibrary.js
+++ src/data/myLibrary.js
@@ -8,7 +8,9 @@ export const mySeries = [
     capitulos: {
       "4": [
         { id: 1, titulo: "Episodio 1", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipOjG1aeozRH08w9fpsc-_VdSLRRXrO9EgSZXky3?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
-        { id: 2, titulo: "Episodio 2", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM216cR8RmNY9MDFcRfCaIUNWxnrPk13btIjSa7?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" }
+        { id: 2, titulo: "Episodio 2", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM216cR8RmNY9MDFcRfCaIUNWxnrPk13btIjSa7?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+        { id: 3, titulo: "Episodio 3", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipM64LegJzut_wX6sIkOm07VRK7eqSaNR1vkAPym?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" },
+        { id: 4, titulo: "Episodio 4", url: "https://photos.google.com/share/AF1QipPVKUDmoOPRZTEYADXOXC_jv90nh_R9JbyqimZcBOpQtp_k0CcuuUFHjFypxoeQSw/photo/AF1QipNX0Bz5F0Y-_rtAQohgYUq6GoujfJjEWBAuTUxF?key=VHA2SjR5XzhJVEtITFMzaTBMMkpVMlFSdmNqZEdn" }
       ]
     }
   },
@@ -26,6 +28,6 @@ export const mySeries = [
     id: "19995", 
     tmdbId: "19995",
     type: "movie",
-    url: "https://photos.google.com/search/ChdBZ3JlZ2Fkb3MgcmVjaWVudGVtZW50ZSIIEgYKBHICCgAo3pTP19Uz/photo/AF1QipOz_RHrWy_TmsHwQfqFgTA7__1GlTmpJWRYHhFL" // Link específico para Avatar
+    url: "https://photos.google.com/search/ChdBZ3JlZ2Fkb3MgcmVjaWVudGVtZW50ZSIIEgYKBHICCgAo3pTP19Uz/photo/AF1QipOz_RHrWy_TmsHwQfqFgTA7__1GlTmpJWRYHhFL"
   }
 ];
